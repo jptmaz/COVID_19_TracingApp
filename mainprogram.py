58cdd98af24e3b760e68f4c88958f81b8d5dfcdd
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 19:27:16 +0800

    Change column

--- mainprogram.py
+++ mainprogram.py
@@ -4,7 +4,7 @@ from tkinter import *
 C19_Tracer = Tk()
 C19_Tracer.title("COVID 19 TRACER")
 
-login_signup_button = Button(C19_Tracer, text="Log in/Sign up").grid(row="1", column="0")
+login_signup_button = Button(C19_Tracer, text="Log in/Sign up").grid(row="1", column="7")
 
 C19_Tracer.mainloop()
 
