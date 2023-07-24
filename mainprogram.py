fd10510fd9b2457505b14b8949e838ca32b2b8b7
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 19:35:04 +0800

    mainprogram buttons added

--- mainprogram.py
+++ mainprogram.py
@@ -5,7 +5,10 @@ C19_Tracer = Tk()
 C19_Tracer.title("COVID 19 TRACER")
 C19_Tracer.geometry("700x500")
 
-mainscreen_login_signup = Label(C19_Tracer, text = "Sign in:").grid(row = 2, column = 3)
+mainscreen_login_signup = Label(C19_Tracer, text = "Sign in:").grid(row = 0, column = 1)
+mainscreen_login = Button(C19_Tracer, text = "Log in").grid(row = 0, column = 1)
+mainscreen_login = Button(C19_Tracer, text = "Log in").grid(row = 0, column = 1)
+
 
 C19_Tracer.mainloop()
 
