a9aad41759634255b93a2c567fcb64dac4bc2f3c
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 19:26:31 +0800

    New changes

---
+++ mainprogram.py
@@ -0,0 +1,10 @@
+from tracer import *
+from tkinter import *
+
+C19_Tracer = Tk()
+C19_Tracer.title("COVID 19 TRACER")
+
+login_signup_button = Button(C19_Tracer, text="Log in/Sign up").grid(row="1", column="0")
+
+C19_Tracer.mainloop()
+
