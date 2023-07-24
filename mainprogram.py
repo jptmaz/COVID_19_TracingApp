6ef31219ac43d6c4383b44d7f668eedda75adcce
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 19:36:06 +0800

    Columns and rows

--- mainprogram.py
+++ mainprogram.py
@@ -6,8 +6,8 @@ C19_Tracer.title("COVID 19 TRACER")
 C19_Tracer.geometry("700x500")
 
 mainscreen_login_signup = Label(C19_Tracer, text = "Sign in:").grid(row = 0, column = 1)
-mainscreen_login = Button(C19_Tracer, text = "Log in").grid(row = 0, column = 1)
-mainscreen_login = Button(C19_Tracer, text = "Log in").grid(row = 0, column = 1)
+mainscreen_login = Button(C19_Tracer, text = "Log in").grid(row = 0, column = 2)
+mainscreen_signup = Button(C19_Tracer, text = "Log in").grid(row = 0, column = 2)
 
 
 C19_Tracer.mainloop()
