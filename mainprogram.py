714c7da48fde1e1415a777873431f4c408e08561
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 19:28:06 +0800

    Geometry added

--- mainprogram.py
+++ mainprogram.py
@@ -3,6 +3,7 @@ from tkinter import *
 
 C19_Tracer = Tk()
 C19_Tracer.title("COVID 19 TRACER")
+C19_Tracer.geometry("700x500")
 
 login_signup_button = Button(C19_Tracer, text="Log in/Sign up").grid(row="1", column="7")
 
