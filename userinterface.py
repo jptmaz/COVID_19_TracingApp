13f9aae34430e974f33d6f7379e8c190828aa38e
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 16:44:26 +0800

    Trial and Error

--- userinterface.py
+++ userinterface.py
@@ -3,30 +3,17 @@ import tkinter
 from tkinter import *
 from functools import partial
 
-def user_login(self, username, password):
-    print("Username: ", username.get())
-    print("Password: ", password.get())
-    return
-
-#window
-Window = Tk()
-Window.geometry('400x150')
-Window.title("COVID 19 - Tracer")
-
-#username
-username_label = Label(Window, text = "Username").grid(row=0, column = 0)
-username = StringVar()
-username_entry = Entry(Window, textvariable= username).grid(row=0, column=1)
-
-#password
-password_label = Label(Window, text = "Password").grid(row=1, column=0)
-password = StringVar()
-password_entry = Entry(Window, textvariable="Password", show="*").grid(row=1, column=1)
-
-#validate login
-validate_login = partial(user_login, username, password)
-
-#login button
-login_button = Button(Window, text = "Login", command=validate_login).grid(row=4, column=0)
-
-Window.mainloop()
+class user_input():
+    def main_screen():
+        main_screen = Tk()
+        main_screen.geometry("400x150")
+        main_screen.title("COVID 19 - TRACER (LOGIN)")
+        
+        Label(text="Log in or Register")
+        Label(text="").pack()
+        
+        Button(text="Register", height="2", width="30").pack()
+        
+        main_screen.mainloop()
+        
+user_input()
