d8254a91d19fc47026decd13866a61449e38e8fb
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 16:25:19 +0800

    Commit

--- userinterface.py
+++ userinterface.py
@@ -17,9 +17,10 @@ Window.title("COVID 19 - Tracer")
 username_label = Label(Window, text = "Username").grid(row=0, column = 0)
 username = StringVar()
 username_entry = Entry(Window, textvariable= username).grid(row=0, column=1)
-Window.mainloop()
 
 #password
 password_label = Label(Window, text = "Password").grid(row=0, column=1)
 password = StringVar()
 password_entry = Entry(Window, textvariable="Password").grid(row=0, column=1)
+
+Window.mainloop()
