63bfb2085bf16e7fd0fbd4dda4106a3c8aecb2ec
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 19:51:29 +0800

    Changes added

--- userinterface.py
+++ userinterface.py
@@ -2,10 +2,17 @@ import csv
 from tkinter import *
 
 class User_Input:
-    def log_in():
-        Window = Tk()
-        Window.geometry("400x150")
-        Window.title("COVID 19 - TRACER (LOGIN)")
-
-
-User_Input.log_in()
+    def log_in(self, username, password):
+        self.Login_Window = Tk()
+        self.Login_Window.geometry("400x150")
+        self.Login_Window.title("Log in")
+        self.username = username
+        self.password = password
+        Window = self.Login_Window
+        
+        username_label =Label(self.Login_Window, text = "Username").grid(row=0, column=1)
+        username = StringVar()
+        password_entry = Entry(self.Login_Window, textvariable="Username").grid(row=2, column=1)
+        
+        Window.mainloop()
+        return
