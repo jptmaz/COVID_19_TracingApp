90248dfa791ba7194c402c415b39ad4e31353fa6
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 09:38:39 +0800

    Pseudocode for interface

--- userinterface.py
+++ userinterface.py
@@ -1,8 +1,6 @@
+#Import tkinker and cvs
 from tkinter import *
+import cvs
 
-Window = Tk()
-entry = Entry()
-entry.pack()
-entry.config(bg = '#b21c0e')
-entry.config(fg = '#470a0a')
-Window.mainloop()
+#Create Class for the User Interface
+#Add input details
