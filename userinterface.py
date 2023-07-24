d2d39c483b12bd465d1225dcae52437ed75bba76
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 20:36:00 +0800

    Testing

--- userinterface.py
+++ userinterface.py
@@ -1,25 +1,16 @@
-#import tools for interface
 from tkinter import *
 
-#make class
-class user_interface():
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        
-    #make log in
-    def log_in(self, username, password):
-        Window = Tk()
-        Window.title("Log in")
-        Window.geometry("150x150")
-        #username
-        username_label = Label(Window, text = "Username").grid(row=0, column=0)
-        username = StringVar()
-        username_entry = Entry(Window, textvariable="Username").grid(row=0, column =1)
-        #password
-        password_label = Label(Window, text = "Password").grid(row=1, column=0)
-        password = StringVar()
-        password_entry = Entry(Window, textvariable="Password", show="*").grid(row=1, column =1)
+Window = Tk()
+Window.title("Log in")
+Window.geometry("150x150")
+#username
+username_label = Label(Window, text = "Username").grid(row=0, column=0)
+username = StringVar()
+username_entry = Entry(Window, textvariable="Username").grid(row=0, column =1)
+#password
+password_label = Label(Window, text = "Password").grid(row=1, column=0)
+password = StringVar()
+password_entry = Entry(Window, textvariable="Password", show="*").grid(row=1, column =1)
 
 
         
