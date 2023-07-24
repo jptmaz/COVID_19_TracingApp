a4cbbb9613fc8c64d55b36cefdfb2143a4f312c7
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 20:22:20 +0800

    Starting from scratch 3

--- userinterface.py
+++ userinterface.py
@@ -1,6 +1,11 @@
 from tkinter import *
 
 UI_Window = Tk()
+UI_Window.title("Log in/Sign up")
+UI_Window.geometry("700x500")
+
+Login_Button = Button(text= "Log in").grid(row = 1, column = 1)
+Signup_Button = Button(text= "Sign up").grid(row = 1, cplumn = 2)
 
 UI_Window.mainloop()
         
