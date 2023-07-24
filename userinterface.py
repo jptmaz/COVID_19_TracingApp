48594cad9c10ca366a8b100148cce3b6c7747a8c
jptmaz <jaspagmaz98@gmail.com>
2023-07-24 17:26:50 +0800

    New

--- userinterface.py
+++ userinterface.py
@@ -1,19 +1,4 @@
-import tkinter
-
+import csv
 from tkinter import *
-from functools import partial
 
-class user_input():
-    def main_screen():
-        main_screen = Tk()
-        main_screen.geometry("400x150")
-        main_screen.title("COVID 19 - TRACER (LOGIN)")
-        
-        Label(text="Log in or Register")
-        Label(text="").pack()
-        
-        Button(text="Register", height="2", width="30").pack()
-        
-        main_screen.mainloop()
-        
-user_input()
+
