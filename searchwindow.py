df57001cfac78f67410c96763126fccd6d04b991
jptmaz <jaspagmaz98@gmail.com>
2023-07-27 23:00:27 +0800

    Progress #2

--- searchwindow.py
+++ searchwindow.py
@@ -1,9 +1,12 @@
 from tkinter import *
+import tkinter as tk  
+from tkinter import ttk
+from tkinter import filedialog 
 from functools import partial
 
 class hatdog():
     
-    def searchresult(inputSdbase):
+    def searchresult(label_result, inputSdbase):
         resDdbase = (inputSdbase.get())
          # create list of inputs
         print(resDdbase)
@@ -20,29 +23,34 @@ class hatdog():
                         print('Line:', line)
 
     def cheese():
-        searchroot = Tk()  
+        searchroot = tk.Tk()  
         searchroot.geometry('1024x600')  
         searchroot.title('Covid 19 Contact - Search Menu')
         
-        sdbase = StringVar()
+        sdbase = tk.StringVar()
         
-        labelsearch= Label(searchroot, text="Search database for word").grid(row=3, column=0)
-        entrysearch = Entry(searchroot, textvariable=sdbase).grid(row=3, column=2)
+        labelsearch= tk.Label(searchroot, text="Search database for word").grid(row=3, column=0)
+        entrysearch = tk.Entry(searchroot, textvariable=sdbase).grid(row=3, column=2)
+     
+        print(sdbase)
+        print("^^^ After submit sdbase")
         
+        labelResult = tk.Label(searchroot)   
+        labelResult.grid(row=20, column=0) 
         
-        hatdog.searchresult = partial(hatdog.searchresult, sdbase)
+        hatdog.searchresult = partial(hatdog.searchresult, labelResult, sdbase)
         #buttonsubmit
-        buttonCal = Button(searchroot, text="Search", command=hatdog.searchresult).grid(row=4, column=0)
+        buttonCal = tk.Button(searchroot, text="Search", command=hatdog.searchresult).grid(row=4, column=0)
         
         
         
-        l1 = Label(searchroot,  text='Database', width=0 ) # added one Label 
+        l1 = tk.Label(searchroot,  text='Database', width=0 ) # added one Label 
         l1.grid(row=20,column=0) 
         
         
 
 #Not working
-        t1 = Text(searchroot,  height=10, width=20) # added one text box
+        t1 = tk.Text(searchroot,  height=10, width=20) # added one text box
         t1.grid(row=20,column=5) 
 
         #with open("dBase.txt", 'r') as f:    
